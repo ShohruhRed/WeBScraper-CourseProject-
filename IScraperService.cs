9504db9b7dc53c73360a3776b333ce17f00ec10c
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-05-08 22:27:38 +0300

    +implemented a backend for the remaining pages

---
+++ IScraperService.cs
@@ -0,0 +1,12 @@
+﻿namespace WeBScraper_CourseProject_
+{
+    public interface IScraperService
+    {
+        public Task<List<News>> BelarusNewsScraper();
+        public Task<List<News>> UzbekistanNewsScraper();
+        public Task<List<News>> SportNewsScraper();
+        public Task<List<News>> GamingNewsScraper();
+        public Task<List<News>> MusicNewsScraper();
+        public Task<List<News>> MovieNewsScraper();
+    }
+}
--- ScraperService.cs
+++ ScraperService.cs
@@ -6,7 +6,7 @@ using WeBScraper_CourseProject_.Data;
 
 namespace WeBScraper_CourseProject_
 {
-    public class ScraperService
+    public class ScraperService : IScraperService
     {
         private readonly ApplicationDbContext _context;
 
@@ -14,7 +14,29 @@ namespace WeBScraper_CourseProject_
         {
             _context = context;
         }
-        public async Task<List<News>> BelarusNewsScrapping()
+
+        public async Task<List<News>> GetDbNews()
+        {
+            return await _context.NewsDb.ToListAsync();
+
+        }
+
+        public async Task<ActionResult<List<News>>> AddNews(News news)
+        {
+            try
+            {
+                await _context.NewsDb.AddAsync(news);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return await _context.NewsDb.ToListAsync();
+
+        }
+
+        public async Task<List<News>> BelarusNewsScraper()
         {
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
@@ -40,28 +62,153 @@ namespace WeBScraper_CourseProject_
             }
 
             return jobs;
-        }
+        }      
 
-        public async Task<List<News>> GetDbNews()
+        public async Task<List<News>> UzbekistanNewsScraper()
         {
-            return await _context.NewsDb.ToListAsync();
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            List<News> jobs = new List<News>();
 
+            var web = new HtmlWeb();
+
+
+            var doc = await web.LoadFromWebAsync("https://www.spot.uz/ru/news/");
+            foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='contentBox']"))
+            {
+
+                string title = item.SelectSingleNode(".//h2[@class='itemTitle']").InnerText.Trim();
+                string details = "https://www.spot.uz/" + item.SelectSingleNode(".//a").GetAttributeValue("href", null).Trim();
+                string img = "https://www.spot.uz/" + item.SelectSingleNode(".//img").GetAttributeValue("data-src", null).Trim();
+
+                jobs.Add(new News()
+                {
+                    Title = title,
+                    Details = details,
+                    Img = img,
+
+                }); ;
+            }
+
+            return jobs;
         }
 
-        public async Task<ActionResult<List<News>>> AddNews(News news)
+        public async Task<List<News>> SportNewsScraper()
         {
-            try
-            {
-                await _context.NewsDb.AddAsync(news);
-                await _context.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return await _context.NewsDb.ToListAsync();
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            List<News> jobs = new List<News>();
 
+            var web = new HtmlWeb();
+
+
+            var doc = await web.LoadFromWebAsync("https://www.mk.ru/sport/");
+            foreach (var item in doc.DocumentNode.SelectNodes("//li[@class='article-listing__item']"))
+            {
+
+                string title = item.SelectSingleNode(".//h3[@class='listing-preview__title']").InnerText.Trim();
+                string details = item.SelectSingleNode(".//a").GetAttributeValue("href", null).Trim();
+                string img = item.SelectSingleNode(".//img").GetAttributeValue("src", null).Trim();
+
+                jobs.Add(new News()
+                {
+                    Title = title,
+                    Details = details,
+                    Img = img,
+
+                });
+            }
+
+            return jobs;
         }
 
+        public async Task<List<News>> GamingNewsScraper()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            List<News> jobs = new List<News>();
+
+            var web = new HtmlWeb();
+
+
+            var doc = await web.LoadFromWebAsync("https://www.playground.ru/news");
+            foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='post']"))
+            {
+
+                string title = item.SelectSingleNode(".//div[@class='post-title']").InnerText.Trim();
+                string details = item.SelectSingleNode(".//a").GetAttributeValue("href", null).Trim();
+                string img = item.SelectSingleNode(".//img").GetAttributeValue("src", null).Trim();
+
+                jobs.Add(new News()
+                {
+                    Title = title,
+                    Details = details,
+                    Img = img,
+
+                }); ;
+            }
+
+            return jobs;
+        }
+
+        public async Task<List<News>> MusicNewsScraper()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            List<News> jobs = new List<News>();
+
+            var web = new HtmlWeb();
+
+
+            var doc = await web.LoadFromWebAsync("https://www.mk.ru/culture/zd/");
+            foreach (var item in doc.DocumentNode.SelectNodes("//li[@class='article-listing__item']"))
+            {
+
+                string title = item.SelectSingleNode(".//h3[@class='listing-preview__title']").InnerText.Trim();
+                string details = item.SelectSingleNode(".//a").GetAttributeValue("href", null).Trim();
+                string img = item.SelectSingleNode(".//img").GetAttributeValue("src", null).Trim();
+
+                jobs.Add(new News()
+                {
+                    Title = title,
+                    Details = details,
+                    Img = img,
+
+                }); ;
+            }
+
+            return jobs;
+        }
+
+        public async Task<List<News>> MovieNewsScraper()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            List<News> jobs = new List<News>();
+
+            var web = new HtmlWeb();
+
+
+            var doc = await web.LoadFromWebAsync("https://www.kinomania.ru/news");
+            foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='pagelist-item clear']"))
+            {
+
+                string title = item.SelectSingleNode(".//div[@class='pagelist-item-title']").InnerText.Trim();
+                //string text = item.SelectSingleNode("//div[@class='pagelist-item clear']/p").InnerText.Trim();
+                string details = "https://www.kinomania.ru/" + item.SelectSingleNode(".//a").GetAttributeValue("href", null).Trim();
+                string img = item.SelectSingleNode(".//img").GetAttributeValue("data-original", null).Trim();
+
+                jobs.Add(new News()
+                {
+                    Title = title,
+                    //Text = text,
+                    Details = details,
+                    Img = img,
+
+                }); ;
+            }
+
+            return jobs;
+        }
     }
 }
