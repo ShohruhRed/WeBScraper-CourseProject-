b8f75a49e3406dbe0db3f419bf5af4370ce1bd7e
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-05-21 18:20:04 +0300

    + twitter oauth implemented

--- Program.cs
+++ Program.cs
@@ -47,8 +47,14 @@ builder.Services.AddAuthentication()
        .GetValue<string>("ClientId");
        options.ClientSecret = builder.Configuration.GetSection("AuthenticationFB")
        .GetValue<string>("ClientSecret");
-   }
-   );
+   })
+   .AddTwitter(options =>
+   {
+       options.ConsumerKey = builder.Configuration.GetSection("AuthenticationTwitter")
+       .GetValue<string>("ConsumerKey");
+       options.ConsumerSecret = builder.Configuration.GetSection("AuthenticationTwitter")
+       .GetValue<string>("ConsumerSecret");
+   });
    
 
 var app = builder.Build();
